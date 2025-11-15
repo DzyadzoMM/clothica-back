a8fb820467d5675b77503abc111f2c4e468e55ce
Yuri <114495144+d-yv@users.noreply.github.com>
2025-11-15 22:19:36 +0200

    Revert "Order"

--- src/validation/ordersValidation.js
+++ src/validation/ordersValidation.js
@@ -8,30 +8,26 @@ const objectIdValidator = (value, helpers) => {
     return !isValidId ? helpers.message("Invalid ID value!") : value;
 };
 
-export const cartItemJoiObject = Joi.object({
-    goodId: Joi.string().custom(objectIdValidator).required(),
-
-    pricePerItem: Joi.number().positive().required(), 
-    size: Joi.string().valid(...SIZES),
-    amount: Joi.number().integer().positive().min(1).default(1),
-});
-
 export const cartItemSchema = {
-    [Segments.BODY]: cartItemJoiObject
+    [Segments.BODY]: Joi.object({
+        goodId: Joi.string().custom(objectIdValidator).required(),
+        size: Joi.string().valid(...SIZES),
+        amount: Joi.number().integer().positive().min(1).default(1),
+    })
 };
 
 export const createOrderSchema = {
     [Segments.BODY]: Joi.object({
-    
-        cart: Joi.array().items(cartItemJoiObject).min(1).required(), 
+        cart: Joi.array().items(cartItemSchema).min(1).required(),
         status: Joi.string().valid(...STATUS).required(),
-        userData: Joi.object()
+        userData: Joi.object({
+            userId: Joi.string().custom(objectIdValidator).required(),
             firstName: Joi.string().trim().required(),
             lastName: Joi.string().trim().required(),
             phone: Joi.string().trim().required(),
             city: Joi.string().trim().required(),
             postOfficeNum: Joi.string().trim().required(),
             comment: Joi.string().trim().allow("").max(500).optional(),
-        }).required() 
+        })
     })
 };
