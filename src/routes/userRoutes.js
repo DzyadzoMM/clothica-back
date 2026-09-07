822671c95813a654b6cfdac715a5bf17fa88d312
Myroslav Dzyadzo <122197686+DzyadzoMM@users.noreply.github.com>
2026-09-07 13:59:44 +0300

    Add route for updating push token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   getCurrentUser,
   updateCurrentUser,
+  updatePushToken,
 } from '../controllers/userController.js';
 import { authenticate } from '../middleware/authenticate.js';
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.get('/api/users/me', authenticate, getCurrentUser);
 router.patch('/api/users/me', authenticate, updateCurrentUser);
+router.post('/api/users/push-token', authenticate, updatePushToken);
 
 export default router;
